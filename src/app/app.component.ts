77c403e7b88cfa162ba494494b63a786a0d97b96
Ahmed Girach <ahmedkhatri6@gmail.com>
2018-02-16 23:45:31 +0530

    [ADD] Initial structure of the App

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "With the help of this project (made in Angular 5), you'll be able to search People on GitHub.";
+}
