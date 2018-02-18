3d6c1fa11422f7d37e164f947fc71c2e4cae1185
Ahmed Girach <ahmedkhatri6@gmail.com>
2018-02-18 17:16:37 +0530

    [IMP] Removed enter concept, other view improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,42 +4,27 @@ import { GithubService } from "./github.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   userName: string;
   user: any[];
-  enterHasBeenPressed;
 
-  constructor(private _service: GithubService) {}
+  constructor(private _service: GithubService) {
+    this.userName = "";
+  }
 
   ngOnInit() {
     const input: HTMLElement = document.getElementsByClassName(
       "input"
     )[0] as HTMLElement;
     input.focus();
-    this.hide("not_found");
-  }
-
-  fetchData() {
-    this._service.getUser().subscribe(user => {
-      this.user = user;
-    });
   }
 
   searchUser(e) {
-    if (e.key === "Enter") {
-      this._service.setUser(this.userName);
-      this.fetchData();
-      this.enterHasBeenPressed = true;
-    }
-    if (this.enterHasBeenPressed) {
-      console.info("enterHasBeenPressed =>", this.enterHasBeenPressed);
-      // @ToDo: Stuff for hide/show...
-    }
-  }
-
-  hide(element: string) {
-    document.getElementById(element).style.display = "none";
+    this._service.setUser(this.userName);
+    this._service.getUser().subscribe(user => {
+      this.user = user;
+    });
   }
 }
