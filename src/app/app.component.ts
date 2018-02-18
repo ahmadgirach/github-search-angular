ed64dc650af53866562b780b3cbad52cc6d3b10b
Ahmed Girach <ahmedkhatri6@gmail.com>
2018-02-18 14:54:04 +0530

    [IMP] view/other enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { GithubService } from "./github.service";
 
 @Component({
@@ -9,8 +9,17 @@ import { GithubService } from "./github.service";
 export class AppComponent {
   userName: string;
   user: any[];
+  enterHasBeenPressed;
 
   constructor(private _service: GithubService) {}
+
+  ngOnInit() {
+    const input: HTMLElement = document.getElementsByClassName(
+      "input"
+    )[0] as HTMLElement;
+    input.focus();
+    this.hide("not_found");
+  }
 
   fetchData() {
     this._service.getUser().subscribe(user => {
@@ -19,9 +28,18 @@ export class AppComponent {
   }
 
   searchUser(e) {
-    if (e.which == 13) {
+    if (e.key === "Enter") {
       this._service.setUser(this.userName);
       this.fetchData();
+      this.enterHasBeenPressed = true;
     }
+    if (this.enterHasBeenPressed) {
+      console.info("enterHasBeenPressed =>", this.enterHasBeenPressed);
+      // @ToDo: Stuff for hide/show...
+    }
+  }
+
+  hide(element: string) {
+    document.getElementById(element).style.display = "none";
   }
 }
